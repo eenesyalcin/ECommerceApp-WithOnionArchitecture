d22187585c817ec72d8e10ac0dc5cb0079b439ec
eenesyalcin <eenes.ylcn@gmail.com>
2025-02-19 15:08:15 +0300

    It was implemented into the Onion Architecture backend project and tested by simulation.

---
+++ ECommerceServer/Core/ECommerceServer.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceServer.Domain.Entities;
+
+namespace ECommerceServer.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
---
+++ ECommerceServer/Infrastructure/ECommerceServer.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceServer.Application.Abstractions;
+using ECommerceServer.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerceServer.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
