aab714c5d24fb61fb472a7803becb8db9fabd9bd
eenesyalcin <eenes.ylcn@gmail.com>
2025-02-24 14:50:18 +0300

    Angular Material infrastructure integration was done.

--- ECommerceClient/src/app/admin/components/layout/layout.component.ts
+++ ECommerceClient/src/app/admin/components/layout/layout.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav'; 
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet ,HeaderComponent, SidebarComponent, FooterComponent],
+  imports: [RouterOutlet ,HeaderComponent, SidebarComponent, FooterComponent, MatSidenavModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
--- ECommerceClient/src/app/admin/components/layout/sidebar/sidebar.component.ts
+++ ECommerceClient/src/app/admin/components/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatListModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
