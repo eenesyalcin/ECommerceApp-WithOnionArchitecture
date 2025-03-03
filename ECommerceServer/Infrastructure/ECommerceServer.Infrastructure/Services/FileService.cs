e09e7447eac24420eab6cb55b8392cae5b00fd1b
eenesyalcin <eenes.ylcn@gmail.com>
2025-03-04 01:08:52 +0300

    Operations were carried out to save file names in SEO compatible manner.

--- ECommerceServer/Infrastructure/ECommerceServer.Infrastructure/Services/FileService.cs
+++ ECommerceServer/Infrastructure/ECommerceServer.Infrastructure/Services/FileService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ECommerceServer.Application.Services;
+using ECommerceServer.Infrastructure.Operations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
@@ -23,7 +24,7 @@ namespace ECommerceServer.Infrastructure.Services
             {
                 await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
 
-                await fileStream.CopyToAsync(fileStream);
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
                 return true;
             }
@@ -34,10 +35,68 @@ namespace ECommerceServer.Infrastructure.Services
             }
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+        async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
         {
-            
+            string extension = Path.GetExtension(fileName);
+            string newFileName = string.Empty;
+
+            if (first)
+            {
+                string oldName = Path.GetFileNameWithoutExtension(fileName);
+                newFileName = $"{NameOperations.CharacterRegulatory(oldName)}{extension}";
+            }
+            else
+            {
+                newFileName = fileName;
+                int indexNo1 = newFileName.IndexOf('-');
+
+                if (indexNo1 == -1)
+                {
+                    newFileName = $"{Path.GetFileNameWithoutExtension(fileName)}-2{extension}";
+                }
+                else
+                {
+                    int lastIndex = 0;
+                    while (true)
+                    {
+                        lastIndex = indexNo1;
+                        indexNo1 = newFileName.IndexOf("-", indexNo1 + 1);
+                        if (indexNo1 == -1)
+                        {
+                            indexNo1 = lastIndex;
+                            break;
+                        }
+                    }
+
+                    int indexNo2 = newFileName.IndexOf(".");
+                    if (indexNo2 == -1)
+                    {
+                        newFileName = $"{Path.GetFileNameWithoutExtension(fileName)}-2{extension}";
+                    }
+                    else
+                    {
+                        string fileNo = newFileName.Substring(indexNo1 + 1, indexNo2 - indexNo1 - 1);
+                        if (int.TryParse(fileNo, out int _fileNo))
+                        {
+                            _fileNo++;
+                            newFileName = newFileName.Remove(indexNo1 + 1, indexNo2 - indexNo1 - 1).Insert(indexNo1 + 1, _fileNo.ToString());
+                        }
+                        else
+                        {
+                            newFileName = $"{Path.GetFileNameWithoutExtension(fileName)}-2{extension}";
+                        }
+                    }
+                }
+            }
+
+            if (File.Exists($"{path}\\{newFileName}"))
+            {
+                return await FileRenameAsync(path, newFileName, false);
+            }
+
+            return newFileName;
         }
+
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
         {
@@ -51,7 +110,7 @@ namespace ECommerceServer.Infrastructure.Services
             List<bool> results = new();
             foreach(IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
+                string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
 
                 bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
                 datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
